2744445a80767db257e463d1e03c8f6bc59b5396
andreastif <45339645+andreastif@users.noreply.github.com>
2023-02-10 16:42:33 +0100

    Övningsuppgifter 4-6

--- src/main/kotlin/classExercises/main.kt
+++ src/main/kotlin/classExercises/main.kt
@@ -190,9 +190,9 @@ fun main() {
 
         fun fibonnacciLoop(mut: MutableList<Int>, target: Int): Int {
             return if (mut.size == target)
-                mut[target-1]
+                mut[target - 1]
             else {
-                mut.add(mut[mut.size-2] + mut[mut.size-1])
+                mut.add(mut[mut.size - 2] + mut[mut.size - 1])
                 fibonnacciLoop(mut, target)
             }
         }
@@ -200,5 +200,142 @@ fun main() {
     }
 
     println(findFibonnacciNum(21))
+
+    //Övningsuppgift 4-a
+
+    //glöm ej val/var i constructor till klassen, annars går det ej att använda/anropa i funktionerna
+    open class Animal(var type: String, val numOfLegs: Int) {
+        fun printType() = println(type)
+        fun printLegs() = println(numOfLegs)
+
+        //run låter oss omdefiniera en funktion eller variabel innanför scopet av den funktion som omsluter dessa.
+        fun allInfo() = run {
+            fun printType() =
+                println("Gris") //omdefinierar till att skriva ut "gris" (har ingenting med >val type< att göra)
+
+            fun printLegs() = println("HOWDY NEIGHBOR") //omdefinierar till att skriva ut "howdy neighbor"
+            printType() //Anropar den LOKALA funktionen annars hör vi ingenting. Det räcker inte med att anropa allInfo. Vi måste anropa internt efter omdefinition.
+            printLegs() //anropar den igen
+        }
+
+        fun allInfo2() {
+            printType()
+            printLegs()
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (other !is Animal) return false
+
+            if (type != other.type) return false
+            if (numOfLegs != other.numOfLegs) return false
+
+            return true
+        }
+
+        override fun hashCode(): Int {
+            var result = type.hashCode()
+            result = 31 * result + numOfLegs
+            return result
+        }
+
+        override fun toString(): String {
+            return "Animal(type='$type', numOfLegs=$numOfLegs)"
+        }
+
+
+    }
+
+    val myTurtle = Animal("turtle", 4)
+    val mySnail = Animal("Snail", 0)
+    val myMonkey = Animal("Monkey", 2)
+    myTurtle.allInfo()
+    myTurtle.allInfo2()
+    mySnail.allInfo2()
+    myMonkey.allInfo2()
+
+    //Övningsuppgift 4-b
+    val myListOfAnimals = mutableListOf(myTurtle, mySnail, myMonkey)
+    for (element in myListOfAnimals) {
+        println("type: ${element.type}, no of legs: ${element.numOfLegs}")
+    }
+
+    //Övningsuppgift 4-c
+    val mySet = mutableSetOf(myTurtle, myTurtle, mySnail, mySnail, myMonkey, myMonkey)
+    val mySet2 = mutableSetOf(myMonkey, myTurtle, mySnail)
+    mySet.forEach{e -> println("set1:$e")}
+    mySet2.forEach{e -> println("set2:$e")}
+    println(mySet == mySet2) //true
+
+    //Övningsuppgift 4-d
+//    I ditt djur-program, skapa upp en map över dina djur
+//    • Låt djurets namn vara nyckel (du hittar på namn till dem)
+//    • Låt själva djurobjektet vara värdet i mappen
+//    • Skriv ut alla djur i din mapp
+    val myMap = mutableMapOf("Ahmed" to myMonkey, "Lisa" to mySnail, "Giorgios" to myTurtle)
+    myMap.forEach{e-> println(e)}
+
+    //Övningsuppgift 4-e
+    println("skriv turtle snail eller monkey")
+    when (readLine()) {
+        "turtle" -> println("blub blub")
+        "snail" -> println("blib blib")
+        "monkey" -> println("ooh ooh aah aah")
+    }
+
+    //Övningsuppgift 5-a
+    data class Rectangle(val width: Int, val height: Int)
+
+    //två olika sätt att skriva extension functions på
+    fun Rectangle.Area(): Int {return this.width * this.height}
+    fun Rectangle.Circumference(): Int = (this.width*2) + (this.height*2)
+
+    val myRec1 = Rectangle(55,11)
+    val myRec2 = Rectangle(55,11)
+
+    //användning av extension function
+    println(myRec1.Area())
+    println(myRec1.Circumference())
+
+    //test av data class equals
+    println(myRec1 == myRec2) //true
+
+    //Övningsuppgift 5-b
+    fun String.DoubleUp() = println("$this $this")
+    "hej".DoubleUp()
+
+    //Övningsuppgift 5-c
+    fun String.printRepeat(index: Int) =  repeat(index) {println("$this")}
+    "hej".printRepeat(5)
+
+    //Övningsuppgift 6-a
+    //olika sätt att skriva en lambda på
+    myMap.forEach{e-> println(e)}
+    myMap.forEach{println({"${it.value}"})}
+    myMap.forEach({e-> println(e.value)})
+    myMap.forEach{e-> println(e)}
+
+    //Övningsuppgift 6-b
+    fun changeAnimal(mutableList: MutableList<Animal>) = mutableList.iterator().forEach { e -> e.type = "frog" }
+
+    //Övningsuppgift 6-c
+    changeAnimal(myListOfAnimals)
+    myListOfAnimals.forEach{ println("$it") }
+    val makeSpider = {animal: Animal -> if (animal.type == "frog" && animal.numOfLegs > 2) animal.type = "spider"}
+    val removeFrog = {animal: Animal ->  animal.type != "frog" }
+    myListOfAnimals.forEach(makeSpider)
+    myListOfAnimals.filter(removeFrog).forEach{ println("$it") }
+
+    //Övningsuppgift 6-d
+    val findCat = {animal: Animal -> if (animal.type == "cat") println("cat is here") else println("cat is not here")}
+    val findSpider = {animal: Animal -> if (animal.type == "spider") println("spider is here") else println("spider is not here")}
+    myListOfAnimals.forEach(findCat)
+    myListOfAnimals.forEach(findSpider)
+
+    //Övningsuppgift 6-e
+    fun getLegs(animal: Animal) : Int = animal.numOfLegs
+
+
+
 
 }
